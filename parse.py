5a5c31d6770831b050c6ab548278d8bcf44ca75b
Bogdan <dbogdan@gmail.com>
2015-02-13 00:12:12 +0200

    Fixed some errors and formatted job_code, email and phone

--- parse.py
+++ parse.py
@@ -25,9 +25,7 @@ def parse(filename):
 
         results = []        
         fin.seek(0)
-        candidate_data = re.findall("(?s)\r\n(\d{2}\t.*>\r\n\t \r\n\r\n)", fin.read())
-        print candidate_data
-        candidate_data = candidate_data[0].strip()
+        candidate_data = re.findall("(?s)\r\n(\d{2}\t.*>\r\n\t \r\n\r\n)", fin.read())[0].strip()
         for match in candidate_data.split("\r\n\t \r\n\r\n"):
             person_data = [item.strip() for item in match.split("\t")]
             try:
@@ -35,6 +33,9 @@ def parse(filename):
             except ValueError:
                 job_code, title, name, phone = person_data
                 email = ""
+            job_code = job_code.lstrip("0")
+            if email: email = email.split()[0]
+            phone = phone.split(" <")[0]    
             results.append("{}\t{}\t{}\t{}\t{}\t{}\t{}".format(
                 institution, fice_code, job_code, title, name, email, phone))
     
@@ -42,11 +43,10 @@ def parse(filename):
 
 
 if __name__ == '__main__':
-    with open("output.txt", "w") as fout:
-        fout.writeline("{}\t{}\t{}\t{}\t{}\t{}\t{}".format(
-            "Institution" ,"ficeCode" ,"jobCode" ,"title" ,"fullName" ,"email" ,"phone")
+    with open("output.tab", "w") as fout:
+        fout.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(
+            "Institution" ,"ficeCode" ,"jobCode" ,"title" ,"fullName" ,"email" ,"phone"))
         for infile in glob.glob("input (*).txt"):
-            print infile
-            [fout.writeline("{}".format(line)) for line in parse(infile)]
-                
-
+            print "Working on file `{}`".format(infile)
+            [fout.write("{}\n".format(line)) for line in parse(infile)] 
+            
