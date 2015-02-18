55945691f1aa92e0da7d6f338b5f0b679f5b9760
Bogdan <dbogdan@gmail.com>
2015-02-18 12:14:45 +0200

    Completed Phase 3 function. Errors received. Needs more work.

--- parse.py
+++ parse.py
@@ -15,7 +15,7 @@ def get_name_and_fice(flines):
         if i == 12:
             institution = line.strip()
         elif not fice_code:
-            match = re.search("FICE Identification: \*(\d*)\*", line)
+            match = re.search("FICE Identification: \*0*(\d*)\*", line)
             if match:
                 fice_code = match.group(1)
         elif institution:
@@ -69,62 +69,90 @@ def parse_people(fh):
                 institution, fice_code, job_code, title, name, email, phone))
     return results
 
-        
 
-def parse_institutions(fh):
+class Re(object):
+    """
+    Enables cascading through multiple regex with if, elif, ..., else
+    """
+    def __init__(self):
+        self.result = None
+    def match(self,pattern,text):
+        self.result = re.match(pattern,text)
+        return self.result
+    def search(self,pattern,text):
+        self.result = re.search(pattern,text)
+        return self.result
+
+        
+def parse_institution_data(fh):
     """
     Get info about people (Phase2)
 
     :input: input file handler (as returned by open)
     """
+    rex = Re()
     flines = fh.readlines()
     institution, fice_code = get_name_and_fice(flines)
     phone, unit_id, hi_off, cal_sys, website = ['' for i in range(5)]
-    est, fees, enroll, aff, c_class = ['' for i in range(5)]
+    established , fees, enroll, aff, c_class = ['' for i in range(5)]
     for line in flines:
         if not phone:
-            match = re.search("Phone: \*(\d*)\*", line)
-            if match:
-                fice_code = match.group(1)
-        elif if not unit_id:
-            match = re.search("Unit ID: \*(\d*)\*")
-            if match:
-                unit_id = match.group(1)
-        elif if not hi_off:
-            match = re.search("Highest Offering: \*(\d*)\*")
-            if match:
-                unit_id = match.group(1)
-        elif if not unit_id:
-            match = re.search("Unit ID: \*(\d*)\*")
-            if match:
-                unit_id = match.group(1)
-        elif if not unit_id:
-            match = re.search("Unit ID: \*(\d*)\*")
-            if match:
-                unit_id = match.group(1)
-        elif if not unit_id:
-            match = re.search("Unit ID: \*(\d*)\*")
-            if match:
-                unit_id = match.group(1)
-        elif if not unit_id:
-            match = re.search("Unit ID: \*(\d*)\*")
-            if match:
-                unit_id = match.group(1)
-    
-        
+            if rex.search("Phone: \*(.*)\*", line):
+                phone = rex.result.group(1)
+        elif not unit_id:
+            if rex.search("Unit ID: \*(\d*)\*", line):
+                unit_id = rex.result.group(1)
+        elif not hi_off:
+            if rex.search("Highest Offering: \*(.*)\*", line):
+                hi_off = rex.result.group(1)
+        elif not cal_sys:
+            if rex.search("Calendar System: \*(.*)\*", line):
+                cal_sys = rex.result.group(1)
+        elif not website:
+            if rex.search("Web Site: \*(.*)\*", line):
+                website = rex.result.group(1)
+        elif not established:
+            if rex.search("Established: \*(\d*)\*", line):
+                established = rex.result.group(1)
+        elif not fees:
+            if rex.search("Annual Undergraduate Tuition and Fees \(In-District\): \*(.*)\*", line):
+                fees = rex.result.result.group(1)
+        elif not enroll:
+            if rex.search("Enrollment: \*(.*)\*", line):
+                enroll = rex.result.group(1)
+        elif not aff:
+            if rex.search("Affiliation: \*(.*)\*", line):
+                aff = rex.result.group(1)
+        elif not c_class:
+            if rex.search("Carnegie Class: \*(.*)\*", line):
+                c_class = rex.result.group(1)
+        else:
+            break
+            
+        return [phone, unit_id, hi_off, cal_sys, website, established, fees, enroll, aff, c_class]
 
+    
 
 if __name__ == '__main__':
-    with open("output-people.tab", "w") as fout:
-        fout.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(
-            "Institution" ,"ficeCode" ,"jobCode" ,"title" ,"fullName" ,"email" ,"phone"))
-        for infile in glob.glob("input (*).txt"):
-                print "Working on file `{}`".format(infile)
-                try:
-                    with open(infile, "r") as fh:
-                        [fout.write("{}\n".format(line)) for line in parse_people(fh)] 
-                except Exception as e:
-                    print "\tERROR: `{}`".format(e)
-                    print "\t!! Skipping input file `{}`...".format(infile)
-                    continue
-            
+    # with open("output-people.tab", "w") as fout:
+    #     fout.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(
+    #         "Institution" ,"ficeCode" ,"jobCode" ,"title" ,"fullName" ,"email" ,"phone"))
+    #     for infile in glob.glob("input (*).txt"):
+    #             print "Working on file `{}`".format(infile)
+    #             try:
+    #                 with open(infile, "r") as fh:
+    #                     [fout.write("{}\n".format(line)) 
+    #                      for line in parse_people(fh)] 
+    #             except Exception as e:
+    #                 print "\tERROR: `{}`".format(e)
+    #                 print "\t!! Skipping input file `{}`...".format(infile)
+    #                 continue
+    with open("output-institutions.tab", "w") as fout:
+        out_line_bp = "\t".join(["{}" for i in range(10)]) + "\n"
+        for infile in  glob.glob("input (*).txt"):
+            print "Working on file `{}`".format(infile)
+            with open(infile, "r") as fh:
+                print parse_institution_data(fh)
+                fout.write(out_line_bp.format(*parse_institution_data(fh))) 
+
+
