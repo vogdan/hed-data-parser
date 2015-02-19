b9d4c8532b6e8a1401c1e098a74ced0ae160fde3
Bogdan <dbogdan@gmail.com>
2015-02-19 21:40:02 +0200

    Fixed Issue #4.

--- parse.py
+++ parse.py
@@ -1,6 +1,7 @@
 import re
 import glob
 import platform
+from argparse import ArgumentParser
 
 
 
@@ -132,33 +133,50 @@ def parse_institution_data(fh):
             
     return [institution, phone, fice_code, unit_id, hi_off, cal_sys, website, established, fees, enroll, aff, c_class]
 
-    
+
 
 if __name__ == '__main__':
-    # with open("output-people.tab", "w") as fout:
-    #     fout.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(
-    #         "Institution" ,"ficeCode" ,"jobCode" ,"title" ,"fullName" ,"email" ,"phone"))
-    #     for infile in glob.glob("input (*).txt"):
-    #             print "Working on file `{}`".format(infile)
-    #             try:
-    #                 with open(infile, "r") as fh:
-    #                     [fout.write("{}\n".format(line)) 
-    #                      for line in parse_people(fh)] 
-    #             except Exception as e:
-    #                 print "\tERROR: `{}`".format(e)
-    #                 print "\t!! Skipping input file `{}`...".format(infile)
-    #                 continue
-    with open("output-institutions.tab", "w") as fout:
-        out_line_bp = "\t".join(["{}" for i in range(12)]) + "\n"
-        fout.write(out_line_bp.format(
-                'InstitutionName', 'Phone', 'FICE Identification', 'Unit ID', 
-                'Highest Offering', 'Calendar System', 'webSite', 'Established',
-                'TuitionAndFees', 'Enrollment', 'Affiliation', 'Carnegie Class'))
-        for infile in  glob.glob("input (*).txt"):
-            print "Working on file `{}`".format(infile)
-            with open(infile, "r") as fh:
-                x =  parse_institution_data(fh)
-                print x
-                fout.write(out_line_bp.format(*x)) 
+
+    arg_parser = ArgumentParser(description='Get person and institution info')
+    arg_parser.add_argument('-gp', '--get_people', help='''Get people info.
+Creates `output-people.tab` in CWD''', action="store_true")
+    arg_parser.add_argument('-gi', '--get_insts', help='''Get institution info.
+Creates `output-institutions.tab` in CWD''', action="store_true")
+    args = arg_parser.parse_args()
+
+    if not args.get_people and not args.get_insts:
+        print "Error: Too few args: -gp and/or -gi must be specified. \n\tUse -h for help."
+        exit(1)
+    if args.get_people:
+        print '\n Getting people...'
+        out_file = 'output-people.tab'
+        with open(out_file, "w") as fout:
+            fout.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(
+                    "Institution", "ficeCode", "jobCode", "title",
+                    "fullName", "email", "phone"))
+            for infile in glob.glob("input (*).txt"):
+                print "Working on file `{}`".format(infile)
+                try:
+                    with open(infile, "r") as fh:
+                        [fout.write("{}\n".format(line)) 
+                         for line in parse_people(fh)] 
+                except Exception as e:
+                    print "\tERROR: `{}`".format(e)
+                    print "\t!! Skipping input file `{}`...".format(infile)
+                    continue
+    if args.get_insts:
+        print "\n Getting institutions... "
+        out_file = 'output-institutions.tab'
+        with open(out_file, "w") as fout:
+            out_line_bp = "\t".join(["{}" for i in range(12)]) + "\n"
+            fout.write(out_line_bp.format(
+                    'InstitutionName', 'Phone', 'FICE Identification', 'Unit ID', 
+                    'Highest Offering', 'Calendar System', 'webSite', 'Established',
+                    'TuitionAndFees', 'Enrollment', 'Affiliation', 'Carnegie Class'))
+            for infile in glob.glob("input (*).txt"):
+                print "Working on file `{}`".format(infile)
+                with open(infile, "r") as fh:
+                    fout.write(out_line_bp.format(*parse_institution_data(fh))) 
+
 
 
